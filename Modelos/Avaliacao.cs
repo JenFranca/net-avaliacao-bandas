5a4ca29c5e369dc54d5359f15ac4b1332314b9ac
JenFranca <120391727+JenFranca@users.noreply.github.com>
2024-01-30 09:56:17 -0300

    Add files via upload

---
+++ Modelos/Avaliacao.cs
@@ -0,0 +1,22 @@
+﻿namespace ScreenSound.Modelos;
+
+
+//declarando uma class internal, apenas essa classe irá conseguir ver ela
+internal class Avaliacao
+{
+    public Avaliacao (int nota)
+    {
+        Nota = nota;
+    }
+
+    public int Nota { get; }
+
+
+    public static Avaliacao Parse (string texto) //metodo estatico - podemos chamar esse metodo direto pelo titulo
+    {
+        int nota = int.Parse(texto); 
+        return new Avaliacao (nota);
+    }
+}
+//Os métodos estáticos são muito importantes e serão bastante utilizados em nossas carreiras como 
+//pessoas desenvolvedoras.  Eles permitem a anexação de funções dentro de um tipo
---
+++ Modelos/Musica.cs
@@ -0,0 +1,33 @@
+﻿﻿using System;
+
+namespace ScreenSound.Modelos;
+
+internal class Musica
+{
+    public Musica(Banda banda, string nome)
+        => (Artista, Nome) = (banda, nome);
+
+    public string Nome { get; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+
+
+    public string DescricaoResumida =>  //lambda, quando é algo que queremos apenas mostrar 'get'
+        $"A música {Nome} pertence à banda {Artista}";
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
